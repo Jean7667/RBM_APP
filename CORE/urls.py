88402893871b9dbf5444eccc5f495b511349a8d5
Jeanbaptiste <27924537+Jean7667@users.noreply.github.com>
2024-03-15 08:29:43 +0000

    added placeholders in each page and checked associated URL

--- CORE/urls.py
+++ CORE/urls.py
@@ -3,9 +3,11 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 
 
+    #remember that the name is the end of the url : host/user_app/signup
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user_app/', include('user_app.urls')),
+    path('user_app/', include('user_app.urls'), name="signup"), 
     path('user_app/', include('django.contrib.auth.urls')), 
     path('', TemplateView.as_view(template_name='home.html'),name='home'), 
 ]
