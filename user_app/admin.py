fe577375cd7dbba580a77762847dbc04e7565dab
Jeanbaptiste <27924537+Jean7667@users.noreply.github.com>
2024-03-16 18:47:06 +0000

    admin py modify each model registered and attributes added

--- user_app/admin.py
+++ user_app/admin.py
@@ -3,13 +3,20 @@ from django.contrib.auth.admin import UserAdmin
 
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+from .models import CustomUser, Skill, Expert
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username', 'is_staff','location','name', ]
+    list_display = ['username','location','name','is_manager','is_customer', 'is_expert']
+
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ['name','category','level']
+class ExpertAdmin(admin.ModelAdmin):
+    list_display = ['user','start_date']
     
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Skill,SkillAdmin)
+admin.site.register(Expert, ExpertAdmin)
     
