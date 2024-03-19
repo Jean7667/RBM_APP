965439b6a13cc530d8500514fc6326da05bb6530
Jeanbaptiste <27924537+Jean7667@users.noreply.github.com>
2024-03-19 17:52:41 +0000

    menu active page fixed starting to add a grid to the home page

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,7 @@
 console.log("JSOK");
 
-// Add active class to the current button (highlight it)
 
-/* const linksMenu = document.querySelectorAll(".link");
-console.log(linksMenu);
-
-linksMenu.forEach((element) => {
-  element.addEventListener("click", function () {
-    linksMenu.forEach((a) => a.classList.remove("active"));
-    this.classList.add("active");
-  });
-});
  */
-
 
 document.querySelectorAll('.nav-link').forEach(link => {
   if(link.href === window.location.href){
