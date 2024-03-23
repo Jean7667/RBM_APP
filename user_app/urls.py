30e8097d42350f21580128e82a2591675ae190d8
Jeanbaptiste <27924537+Jean7667@users.noreply.github.com>
2024-03-23 15:31:21 +0000

    added required urls for delete, edit and submission

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SignUpView, CxProfileView, LogoutView, ExpertListView, DeleteCxProfileView, expert_booking
+from .views import SignUpView, CxProfileView, LogoutView, ExpertListView, DeleteCxProfileView, expert_booking, edit_booking, delete_booking, handle_booking_submission, booking_success
 from . import views
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
@@ -8,5 +8,10 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='conf_logout'),
     path('listexpert/', ExpertListView.as_view(), name='list_expert'),
     path('expert-booking/', views.expert_booking, name='expert_booking'),
-    path('expert/<int:expert_id>/', views.expert_detail, name='expert_detail'),        
+    path('expert/<int:expert_id>/', views.expert_detail, name='expert_detail'),
+    path('book/<int:expert_id>/', views.booking_form, name='booking_form'),
+    path('edit/<int:booking_id>/', views.edit_booking, name='edit_booking'),
+    path('delete/<int:booking_id>/', views.delete_booking, name='delete_booking'),
+    path('book/<int:expert_id>/submit/', views.handle_booking_submission, name='handle_booking_submission'),
+    path('booking-success/', booking_success, name='booking_success'),        
 ]
