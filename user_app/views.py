8b79df1ea4934168426dc787ff36ad67bf1d2c72
Jeanbaptiste <27924537+Jean7667@users.noreply.github.com>
2024-03-23 12:25:15 +0000

    booking_form, Handle_booking_submission view added

--- user_app/views.py
+++ user_app/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Expert
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
-from .forms import CustomUserCreationForm
+from .forms import BookingForm, CustomUserCreationForm, CustomUser
 from django.views import View
-from .models import Expert, Skill
-
+from .models import Expert, Skill, Booking
+from django.http import HttpResponse
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
@@ -19,20 +19,14 @@ class SignUpView(CreateView):
         return super().form_valid(form)
     
     
-    # template_name = 'base.html' start on base html 
- 
- #Making sure the user created from the interface is idendify as a customer
-  
+# template_name = 'base.html' start on base html 
+# Making sure the user created from the interface is idendify as a customer
 # Create, but don't save the new author instance.
-## >>> new_author = f.save(commit=False)  
-#https://docs.djangoproject.com/en/5.0/topics/forms/modelforms/
+# new_author = f.save(commit=False)  
+# https://docs.djangoproject.com/en/5.0/topics/forms/modelforms/
 
-""" render page
-def list (request):
-    return render (request,'')
-"""
 
-## access about page
+# access about page
 def about(request):
     return render(request, 'about.html')
 
@@ -46,10 +40,6 @@ def ConfirmLogout(request):
     return render (request,'registration/logout.html')
     
 
-""" 
-def cxprofile (request):
-    return render (request,'customer/cxprofile.html')
- """
 #view for customer profile CRUD class based view
 class CxProfileView(View):
     cx_profile_template = 'customer/cxprofile.html'
@@ -69,7 +59,6 @@ class CxProfileView(View):
             user.last_name = request.POST.get('lastname', user.last_name)
             user.location = request.POST.get('location', user.location)
             user.email = request.POST.get('email', user.email)
-            
             user.save()
             return redirect('customer_profile')  
         else:
@@ -84,22 +73,8 @@ class DeleteCxProfileView(View):
         else:
             return redirect('unauthorized')
         
-        
 
-"""     def delete(self, request):
-        user = request.user
-        if user.is_authenticated and user.is_customer:
-            if request.method == 'DELETE':
-                #print (delete received)
-                #user = User.objects.get(pk=user.pk)    
-                user.delete()
-                #print("User profile deleted")
-                return redirect('home')
-            else:
-                return redirect('login')  """
-
-#Read        
-
+# Read        
 #Display in cxprofileview
 #fct to show last Customer since created_date 
 #fct last connection 
@@ -127,6 +102,7 @@ class ExpertListView(ListView):
         context['categories'] = Skill.objects.values_list('category', flat=True).distinct()
         return context
 
+#############Expert list ################
 
 """ def ExpertBooking (request):
     return render(request, 'customer/expertbooking.html') """
@@ -136,9 +112,77 @@ def expert_booking(request):
     return render(request, 'customer/expertbooking.html', {'experts': experts})
 
 
-from django.shortcuts import render, get_object_or_404
-from .models import Expert
-
 def expert_detail(request, expert_id):
     expert = get_object_or_404(Expert, id=expert_id)
     return render(request, 'customer/expertdetail.html', {'expert': expert})
+
+######## Booking Section ############
+
+def edit_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    form = BookingForm(request.POST or None, instance=booking)
+    
+    if form.is_valid():
+        form.save()
+        return redirect('booking_detail', booking_id=booking.id)  # Redirect to booking detail page
+        
+    return render(request, 'booking/editbooking.html', {'form': form})
+
+def delete_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    
+    if request.method == 'POST':
+        booking.delete()
+        return redirect('home')  # Redirect to booking list page
+    
+    return render(request, 'booking/deletebooking.html', {'booking': booking})
+ 
+def booking_form(request, expert_id):
+    expert = get_object_or_404(Expert, id=expert_id)
+    
+        # Pre-populate the form with expert's data using the 'initial' argument
+    form = BookingForm(initial={'CustomerUser': expert.user})
+    
+    context = {'expert': expert, 'form': form}
+    return render(request, 'booking/formbooking.html', context)
+
+        
+def handle_booking_submission(request, expert_id):
+    expert = get_object_or_404(Expert, id=expert_id)
+    
+    # Check if the form is being submitted
+    if request.method == 'POST':
+        form = BookingForm(request.POST)  
+        if form.is_valid():
+            booking_data = form.cleaned_data
+            checkin = booking_data['checkin']
+            checkout = booking_data['checkout']
+            location = booking_data['location']
+            notes = booking_data['notes']
+            
+            try:
+                expert_user = expert.user
+                customer_user = request.user
+                
+                # Create a new booking
+                booking = Booking.objects.create(
+                    BookingExpert=expert_user,
+                    CustomerUser=customer_user,
+                    checkin=checkin,
+                    checkout=checkout,
+                    location=location,
+                    notes=notes
+                )
+        # keep this code till no more error message in debug mode    
+                # For example:
+                return redirect('booking_success')  # success page missing
+            except Exception as e:
+                #  Catch exception
+                print("Error creating booking:", e)  
+                return HttpResponse("An error occurred while creating the booking.")  # Debug response
+        else:
+            print("Form is not valid. Errors:", form.errors)  # Debug message
+            return HttpResponse("Form has errors")  # Debug response
+    else:
+        # Return a 404 response if accessed via GET method
+        return HttpResponseNotFound()
