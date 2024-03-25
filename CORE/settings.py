c4d7b07ef2cd189d51c75e32e0eafde2b3875d82
Jeanbaptiste <27924537+Jean7667@users.noreply.github.com>
2024-03-25 22:07:41 +0000

    Setting update

--- CORE/settings.py
+++ CORE/settings.py
@@ -18,7 +18,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-91(qd#ylm0w0q+c$!&w%q)k*f0jrf5d9xt=0qm%oi^srp0l$bh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
