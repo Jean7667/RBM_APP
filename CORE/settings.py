40f5837e4660d1e102c18290988841312fddbdcc
Jeanbaptiste <27924537+Jean7667@users.noreply.github.com>
2024-03-25 21:29:27 +0000

    Setting update

--- CORE/settings.py
+++ CORE/settings.py
@@ -11,9 +11,6 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 
-
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -151,8 +148,14 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfiles'),] """
 
 STATIC_URL = 'static/' #  conjunction with the {% static %} Dev/Prod 
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] #Development ccyle
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") #correct
+
+
+
 
 
 # Default primary key field type
