5e92e768d09cfc13ef8ed889e012891cf953db22
Jeanbaptiste <27924537+Jean7667@users.noreply.github.com>
2024-05-09 20:10:08 +0100

    completed role_service

--- user_app/role_service.py
+++ user_app/role_service.py
@@ -6,13 +6,19 @@ from models import CustomUser, Expert
 import datetime
 
 def update_user_role (user, is_expert, is_customer, is_manager):
+#raise error if both roles are selected.    
 #check the user role profile is_expert is_customer is_manager
+#using get_or_create https://docs.djangoproject.com/en/5.0/ref/models/querysets/
  if is_expert: 
-     #using get_or_create https://docs.djangoproject.com/en/5.0/ref/models/querysets/
- if is_expert and is_customer:
-     raise error (You can't set both is_customer and is_manager)    
      expert_instance, created = Expert.objects.get_or_create(user=user)
-if created:
+#then if created add the start_date and save
+    if created:
     expert_instance.start_date = datetime.date.today()
     expert_instance.save()
     
+ if is_expert and is_customer:
+     raise ValueError("You can't set both is_customer and is_manager")
+ #if user is no longer an expert remove instance
+ else:
+     Expert.objects.filter(user=user).delete()    
+     
