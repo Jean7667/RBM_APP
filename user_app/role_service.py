69dfe9e0e5c3c8b19f5259479a740b06eed09df4
Jeanbaptiste <27924537+Jean7667@users.noreply.github.com>
2024-05-09 19:43:26 +0100

    adding condition to prevent several roles to be selected at once

--- user_app/role_service.py
+++ user_app/role_service.py
@@ -2,9 +2,17 @@
 """
 Update the user role profile is_expert is_customer is_manager
 """
-From models import CustomUser, Expert
+from models import CustomUser, Expert
 import datetime
 
-def update_user_role_profile
+def update_user_role (user, is_expert, is_customer, is_manager):
 #check the user role profile is_expert is_customer is_manager
- if is_expert:
+ if is_expert: 
+     #using get_or_create https://docs.djangoproject.com/en/5.0/ref/models/querysets/
+ if is_expert and is_customer:
+     raise error (You can't set both is_customer and is_manager)    
+     expert_instance, created = Expert.objects.get_or_create(user=user)
+if created:
+    expert_instance.start_date = datetime.date.today()
+    expert_instance.save()
+    
