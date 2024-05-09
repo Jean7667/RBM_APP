815d22e199b755de982e2265eec704758d8e4e86
Jeanbaptiste <27924537+Jean7667@users.noreply.github.com>
2024-05-09 19:11:07 +0100

    started service function to update user role

---
+++ user_app/role_service.py
@@ -0,0 +1,10 @@
+#role_service
+"""
+Update the user role profile is_expert is_customer is_manager
+"""
+From models import CustomUser, Expert
+import datetime
+
+def update_user_role_profile
+#check the user role profile is_expert is_customer is_manager
+ if is_expert:
